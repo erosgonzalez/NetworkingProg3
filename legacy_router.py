a8b33cb7033e1f0d4c1cf21be16bedfb27681ee8
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-04-05 10:29:03 -0700

    updated legacy_router.py

--- legacy_router.py
+++ legacy_router.py
@@ -11,30 +11,34 @@ from mininet.link import TCLink, Intf
 from subprocess import call
 
 def myNetwork():
+	net = Mininet( topo=None, build=False, ipBase='10.0.0.0/8')
 
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
+	info( '*** Adding controller\n' )
+	info( '*** Add switches\n')
+	r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    info( '*** Adding controller\n' )
-    info( '*** Add switches\n')
+	info( '*** Add hosts\n')
+	h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+	h1 = net.addHost('h1', cls=Node, ip='10.0.0.1', defaultRoute=None)
 
-    info( '*** Add hosts\n')
+	info( '*** Add links\n')
+	net.addLink(h1,r1)
+	net.addLink(h2,r1)
 
-    info( '*** Add links\n')
+	info( '*** Starting network\n')
+	net.build()
 
-    info( '*** Starting network\n')
-    net.build()
-    info( '*** Starting controllers\n')
-    for controller in net.controllers:
-        controller.start()
+	info( '*** Starting controllers\n')
+	for controller in net.controllers:
+		controller.start()
 
-    info( '*** Starting switches\n')
+	info('Starting switches\n')
 
-    info( '*** Post configure switches and hosts\n')
+	info( '*** Post configure switches and hosts\n')
 
-    CLI(net)
-    net.stop()
+	CLI(net)
+	net.stop()
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
