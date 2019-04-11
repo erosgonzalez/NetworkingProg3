b0ee592a76eabc2334549398c1f7b61bf76e5942
None <None>
2019-04-11 12:10:50 -0700

    Fixed starting code

--- legacy_router.py
+++ legacy_router.py
@@ -16,44 +16,18 @@ def myNetwork():
 	info( '*** Add switches\n')
 	r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
 	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
-
-<<<<<<< HEAD
-    info( '*** Adding controller\n' )
-    info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls = Node, ip = '0.0.0.0')
-    r1.cmd('sysctl - w net.ipv4.ip_forward=1')
-
-    info( '*** Add hosts\n')
-    h2 = net.addHost('h2', cls = Host, ip = '10.0.0.2', defaultRoute = None)
-    h1 = net.addHost('h1', cls = Host, ip = '10.0.0.1', defaultRoute = None)
-
-    info( '*** Add links\n')
-    net.addLink(h1, r1)
-    net.addLink(h2, r1)
-
-    info( '*** Starting network\n')
-    net.build()
-=======
+	
 	info( '*** Add hosts\n')
-	h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-	h1 = net.addHost('h1', cls=Node, ip='10.0.0.1', defaultRoute=None)
+	h2 = net.addHost('h2', cls = Host, ip = '10.0.0.2', defaultRoute = None)
+	h1 = net.addHost('h1', cls = Host, ip = '10.0.0.1', defaultRoute = None)
 
 	info( '*** Add links\n')
-	net.addLink(h1,r1)
-	net.addLink(h2,r1)
+	net.addLink(h1, r1)
+	net.addLink(h2, r1)
 
 	info( '*** Starting network\n')
 	net.build()
-
-	info( '*** Starting controllers\n')
-	for controller in net.controllers:
-		controller.start()
-
-	info('Starting switches\n')
-
-	info( '*** Post configure switches and hosts\n')
->>>>>>> a8b33cb7033e1f0d4c1cf21be16bedfb27681ee8
-
+	
 	CLI(net)
 	net.stop()
 
